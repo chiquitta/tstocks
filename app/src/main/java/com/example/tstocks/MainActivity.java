0fa51ccea0a0d70bf8b405fcaeadaca882a3a14a
Chiquitta <linyuting@MacBook-Pro.local>
2019-11-05 16:47:26 +0800

    update

--- app/src/main/java/com/example/tstocks/MainActivity.java
+++ app/src/main/java/com/example/tstocks/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.tstocks;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -19,13 +20,17 @@ import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.InterstitialAd;
 import com.google.android.gms.ads.MobileAds;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
     private AdView mAdView;
     private InterstitialAd mInterstitialAd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mAdView = findViewById(R.id.adViewbanner);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
@@ -37,15 +42,30 @@ public class MainActivity extends AppCompatActivity {
 
         final EditText buy = (EditText)findViewById(R.id.buy);
         buy.setText("40");
+        final EditText sell = (EditText)findViewById(R.id.sell);
+        sell.setText("42");
 
         final EditText buyNum = (EditText)findViewById(R.id.buyNum);
         buyNum.setText("1000");
 
+        final EditText sellNum = (EditText)findViewById(R.id.sellNum);
+        sellNum.setText("1000");
+
+        String[] plants = new String[]{
+                "10",
+                "9.5",
+                "6",
+                "0",
+        };
+
         Spinner fee = (Spinner)findViewById(R.id.buyFee);
-        final ArrayAdapter<CharSequence> nAdapter = ArrayAdapter.createFromResource(
-                this, R.array.fees, android.R.layout.simple_spinner_item);
-        nAdapter.setDropDownViewResource(
-                android.R.layout.simple_spinner_dropdown_item);
+//        final ArrayAdapter<CharSequence> nAdapter = ArrayAdapter.createFromResource(
+//                this, R.array.fees, android.R.layout.);
+        final ArrayAdapter<String> nAdapter = new ArrayAdapter<String>(
+                this, R.layout.spinner_item, plants);
+//        nAdapter.setDropDownViewResource(
+//                android.R.layout.spinner_item);
+        nAdapter.setDropDownViewResource(R.layout.spinner_item);
         fee.setAdapter(nAdapter);
         fee.setSelection(2);
         fee.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -63,10 +83,40 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Spinner sFee = (Spinner)findViewById(R.id.sellFee);
+//        final ArrayAdapter<CharSequence> nAdapter = ArrayAdapter.createFromResource(
+//                this, R.array.fees, android.R.layout.);
+        final ArrayAdapter<String> nsAdapter = new ArrayAdapter<String>(
+                this, R.layout.spinner_item, plants);
+//        nAdapter.setDropDownViewResource(
+//                android.R.layout.spinner_item);
+        nsAdapter.setDropDownViewResource(R.layout.spinner_item);
+        sFee.setAdapter(nAdapter);
+        sFee.setSelection(2);
+        sFee.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast toast = Toast.makeText(
+                        MainActivity.this,
+                        nAdapter.getItem(position),
+                        Toast.LENGTH_LONG);
+                toast.show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
         setBuyPrice(buy, buyNum);
         setBFee();
+        setBuyTotal();
+        setSellPrice(sell, sellNum);
+        setSFee();
+        setSellTotal();
 
-        buy.addTextChangedListener(new TextWatcher() {
+        sell.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
@@ -79,7 +129,13 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
-                setBuyPrice(buy, buyNum);
+                if(!sell.getText().toString().matches("")) {
+                    setSellPrice(sell, sellNum);
+                    setSFee();
+                    setSellTotal();
+                } else {
+                    sell.setText("0");
+                }
             }
         });
     }
@@ -90,7 +146,11 @@ public class MainActivity extends AppCompatActivity {
         double buyP = Double.parseDouble(fieldBuy.getText().toString());
         double buyN = Double.parseDouble(fieldBuyNum.getText().toString());
         double buyPrice = buyP * buyN;
-        showText.setText(buyPrice+"");
+//        double price = Math.round(buyPrice);
+//        double price = Double.parseDouble(buyPrice+"");
+        DecimalFormat formatter = new DecimalFormat("###,###");
+//        String formatted = formatter.format(price);
+        showText.setText(Math.round(buyPrice)+"");
     }
     private void setBFee() {
         TextView showText = (TextView)findViewById(R.id.bFee);
@@ -102,7 +162,50 @@ public class MainActivity extends AppCompatActivity {
         if(bFee < 20) {
             bFee = 20;
         }
-        showText.setText(bFee+"");
+        showText.setText(Math.round(bFee)+"");
+    }
+    private  void setBuyTotal() {
+        TextView showText = (TextView)findViewById(R.id.buyTotal);
+        TextView buyPrice = (TextView)findViewById(R.id.buyPrice);
+        TextView bFee = (TextView)findViewById(R.id.bFee);
+        double buyP =  Double.parseDouble(buyPrice.getText().toString());
+        double buyF =  Double.parseDouble(bFee.getText().toString());
+        double buyTotal = buyP + buyF;
+        showText.setText(Math.round(buyTotal)+"");
+    }
+    private void setSellPrice(TextView sell, TextView sellNum) {
+        TextView showText = (TextView)findViewById(R.id.sellPrice);
+        EditText fieldBuy = (EditText)findViewById(R.id.sell);
+        EditText fieldBuyNum = (EditText)findViewById(R.id.sellNum);
+        double sellP = Double.parseDouble(fieldBuy.getText().toString());
+        double sellN = Double.parseDouble(fieldBuyNum.getText().toString());
+        double sellPrice = sellP * sellN;
+//        double price = Math.round(buyPrice);
+//        double price = Double.parseDouble(buyPrice+"");
+        DecimalFormat formatter = new DecimalFormat("###,###");
+//        String formatted = formatter.format(price);
+        showText.setText(Math.round(sellPrice)+"");
+    }
+    private void setSFee() {
+        TextView showText = (TextView)findViewById(R.id.sFee);
+        TextView buyPrice = (TextView)findViewById(R.id.sellPrice);
+        Spinner buyFee = (Spinner)findViewById(R.id.sellFee);
+        double sellP =  Double.parseDouble(buyPrice.getText().toString());
+        double sellF = Double.parseDouble(buyFee.getSelectedItem().toString());
+        double sFee = sellP * 0.001425 * sellF * 0.1;
+        if(sFee < 20) {
+            sFee = 20;
+        }
+        showText.setText(Math.round(sFee)+"");
+    }
+    private  void setSellTotal() {
+        TextView showText = (TextView)findViewById(R.id.sellTotal);
+        TextView buyPrice = (TextView)findViewById(R.id.sellPrice);
+        TextView bFee = (TextView)findViewById(R.id.sFee);
+        double sellP =  Double.parseDouble(buyPrice.getText().toString());
+        double sellF =  Double.parseDouble(bFee.getText().toString());
+        double sellTotal = sellP + sellF;
+        showText.setText(Math.round(sellTotal)+"");
     }
     public void ShowAd(View v) {
         if (mInterstitialAd.isLoaded()) {
